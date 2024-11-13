94a61872baeb7c732128212f80cc7129f271d5fd
deliafH <87489626+deliafH@users.noreply.github.com>
2024-11-13 12:12:41 +0700

    DataForTower

---
+++ Assets/Scripts/Data/TowerData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tower")]
+public class TowerData : ScriptableObject
+{
+    public int id;
+    public string Name, information;
+    public GameObject towerType;
+    public Sprite towerSprite;
+    public int price;
+}
